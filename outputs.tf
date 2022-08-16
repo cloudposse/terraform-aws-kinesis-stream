90ad6700b5698c7ac8712769fdf9bd4638f05969
Dan Meyers <danjbh@users.noreply.github.com>
2022-08-15 17:24:20 -0700

    Fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -5,12 +5,12 @@ output "name" {
 
 output "shard_count" {
   description = "Number of shards provisioned."
-  value       = join(aws_kinesis_stream.default.*.shard_count)
+  value       = join("", aws_kinesis_stream.default.*.shard_count)
 }
 
 output "stream_arn" {
   description = "ARN of the Kinesis stream."
-  value       = join(aws_kinesis_stream.default.*.arn)
+  value       = join("", aws_kinesis_stream.default.*.arn)
 }
 
 output "consumers" {
